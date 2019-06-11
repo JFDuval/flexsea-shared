f5ae84cde5ce1fc91956a8af2d812130ae10a126
Kosta Andoni <kandoni@dephy.com>
2019-06-11 14:38:28 -0400

    [EMBD-242] Add enum to read Session Stats from Re

--- rigid/inc/mnReInterface.h
+++ rigid/inc/mnReInterface.h
@@ -24,7 +24,7 @@
 #define MNRE_PULSE_LEN				15
 #define MNRE_PULSE_WIGGLE			2
 
-enum MN_RE_MSG{MN_BOOTED = 0, NEW_CONFIG};
+enum MN_RE_MSG{MN_BOOTED = 0, NEW_CONFIG, SESSION};
 
 //I2C buffers:
 #define MN_WBUF_SIZE				12	//Re writes to Mn
